ec6024ced34fd475dbc6a7e01db617e85c0f923d
Napadensky <davidnapadensky@gmail.com>
2024-04-08 05:54:43 -0300

    create resourse users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from '@app/common';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule],
+  imports: [DatabaseModule, ConfigModule, UsersModule],
 })
 export class AppModule {}
